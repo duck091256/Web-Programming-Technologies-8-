e8fcd56fc6127af8c5ea08950c707f9496483a0f
duc091256 <ductran091256@gmail.com>
2025-10-14 01:11:41 +0700

    Done Lab_7

---
+++ Lab_7/ch14_ex1/model/category.php
@@ -0,0 +1,41 @@
+<?php
+class Category
+{
+    private $id;
+    private $name;
+
+    // public function __construct($id, $name)
+    // {
+    //     $this->id = $id;
+    //     $this->name = $name;
+    // }
+    
+    // REQUIREMENT 4 
+
+    public function __construct()
+    {
+        $this->id = 0;
+        $this->name = '';
+    }
+
+
+    public function getID()
+    {
+        return $this->id;
+    }
+
+    public function setID($value)
+    {
+        $this->id = $value;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($value)
+    {
+        $this->name = $value;
+    }
+}
---
+++ Lab_7/ch14_ex1/model/category_db.php
@@ -0,0 +1,49 @@
+<?php
+class CategoryDB
+{
+    public function getCategories()
+    {
+        $db = Database::getDB();
+        $query = 'SELECT * FROM categories
+                  ORDER BY categoryID';
+        $statement = $db->prepare($query);
+        $statement->execute();
+
+        $categories = array();
+        foreach ($statement as $row) {
+            // $category = new Category($row['categoryID'],
+            //                          $row['categoryName']);
+
+            //REQUIREMENT 5
+
+            $category = new Category();
+            $category->setID($row['categoryID']);
+            $category->setName($row['categoryName']);
+
+            $categories[] = $category;
+        }
+        return $categories;
+    }
+
+    public function getCategory($category_id)
+    {
+        $db = Database::getDB();
+        $query = 'SELECT * FROM categories
+                  WHERE categoryID = :category_id';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':category_id', $category_id);
+        $statement->execute();
+        $row = $statement->fetch();
+        $statement->closeCursor();
+        // $category = new Category(
+        //     $row['categoryID'],
+        //     $row['categoryName']
+        // );
+        
+        $category = new Category();
+        $category->setID($row['categoryID']);
+        $category->setName($row['categoryName']);
+
+        return $category;
+    }
+}
